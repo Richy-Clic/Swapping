782df6364df2571922088f7d4eb75d55fc600c34
Richy-Clic <richy.deanda@gmail.com>
2022-09-19 01:11:28 -0500

    structuring header in a route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
 import './App.css';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register.jsx';
 import NotFound from './pages/NotFound.jsx'
-import { Home } from './pages/Home.jsx';
+import Home from './pages/Home.jsx';
 import Login from './pages/Login'
 
 
 
-
 function App() {
-
   
-const params = useParams()
-console.log(params);
   return (
     
     <BrowserRouter>
-    <Header />
-    <div className='container'>
-      <Routes>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/home" element={<Home />}/>
-        <Route path="*" element={<NotFound />}/>
-        <Route path="/" element={<Login />}></Route>
-      </Routes>
-    </div>
       
-    <Footer />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Header />}>
+          <Route path="register" element={<Register />}/>
+          <Route path="home" element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Logo from '../assets/img/logo.png'
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div className="row mt-3">
+    <div className="container">
+      <div className="row mt-3">
         <div className="col-6 bg-primary px-4 py-2">
             <h2>My swappings</h2>
         </div>
@@ -13,6 +14,9 @@ export const Home = () => {
         <div className="col-12 bg-warning py-2 px-4">
             <h2>Swapping public list</h2>
         </div>
+      </div>
     </div>
   )
 }
+
+export default Home;
